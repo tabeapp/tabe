782615d8b9472dcd8b9021affc5dbaf435e2275c
Cole Berwick <colejberwick@gmail.com>
2020-12-07 15:12:02 -0500

    saved item I guess

--- Tabe/ContentView.swift
+++ Tabe/ContentView.swift
@@ -6,11 +6,28 @@
 //
 
 import SwiftUI
+import Amplify
+import AmplifyPlugins
+
 
 struct ContentView: View {
+    func performOnAppear(){
+        let item = Todo(name: "Build iOS Application", description: "Build an iOS application using Amplify")
+        Amplify.DataStore.save(item) { result in
+            switch(result) {
+            case .success(let savedItem):
+                print("Saved item: \(savedItem.name)")
+            case .failure(let error):
+                print("Could not save item to datastore: \(error)")
+            }
+        }
+    }
+
     var body: some View {
         Text("Hello, world!")
-            .padding()
+            .onAppear {
+                self.performOnAppear()
+            }
     }
 }
 
