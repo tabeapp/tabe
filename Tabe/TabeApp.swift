1ef892da33849c8f700109fbd8d32f60140fa063
Cole Berwick <colejberwick@gmail.com>
2020-12-07 15:05:33 -0500

    seemed to configure amplify

--- Tabe/TabeApp.swift
+++ Tabe/TabeApp.swift
@@ -7,8 +7,25 @@
 
 import SwiftUI
 
+import Amplify
+import AmplifyPlugins
+
+func configureAmplify() {
+    let dataStorePlugin = AWSDataStorePlugin(modelRegistration: AmplifyModels())
+    do {
+        try Amplify.add(plugin: dataStorePlugin)
+        try Amplify.configure()
+        print("Initialized Amplify");
+    } catch {
+        print("Could not initialize Amplify: \(error)")
+    }
+}
+
 @main
 struct TabeApp: App {
+    public init() {
+        configureAmplify()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
