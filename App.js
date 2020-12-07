edbbf1121f795c9ee668da594122d1531e5064ec
Cole Berwick <colejberwick@gmail.com>
2020-12-07 18:01:29 -0500

    gonna mess around a bit

--- App.js
+++ App.js
@@ -6,86 +6,33 @@
  * @flow strict-local
  */
 
-import React, {useEffect, useState} from 'react';
-import {
-  TextInput,
-  Button,
-  StyleSheet,
-  View,
-  Text,
-} from 'react-native';
+import React, {useState} from 'react';
+import { TextInput, Button, StyleSheet, Alert, SafeAreaView } from 'react-native';
 
-import {listTodos} from './graphql/queries';
 import {withAuthenticator} from 'aws-amplify-react-native';
 
-const initialState = {name: '', description: ''};
 
-//const App: () => React$Node = () => {
 const App = () => {
-  const [formState, setFormState] = useState(initialState);
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    fetchTodos();
-  }, []);
-
-  function setInput(key, value) {
-    setFormState({...formState, [key]: value});
-  }
-
-  async function fetchTodos(){
-    try{
-      const todoData = await API.graphql(graphqlOperation(listTodos));
-      const todos = todoData.data.listTodos.items
-      setTodos(todos)
-    } catch(err){console.log('error fetching todos')}
-
-  }
-
-  async function addTodo(){
-    try{
-      const todo = {...formState};
-      setTodos([...todos, todo]);
-      setFormState(initialState);
-      await API.graphql(graphqlOperation(createTodo, {input, todo}));
-    }
-    catch(err){
-      console.log('error creating todo:', err);
-    }
-  }
+  const [message, setMessage] = useState('');
+  const sendMessageToAppleWatch = () => {
+    Alert.alert(`the message "${message}" has been sent`)
+  };
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <TextInput
-        onChangeText={val => setInput('name', val)}
-        style={styles.input}
-        value={formState.name}
+        onChangeText={setMessage}
+        value={message}
         placeholder="Name"
       />
-      <TextInput
-        onChangeText={val => setInput('description', val)}
-        style={styles.input}
-        value={formState.description}
-        placeholder="Description"
-      />
-      <Button title="Create Todo" onPress={addTodo} />
-      {
-        todos.map((todo, index) => (
-          <View key={todo.id ? todo.id : index} style={styles.todo}>
-            <Text style={styles.todoName}>{todo.name}</Text>
-            <Text>{todo.description}</Text>
-          </View>
-        ))
-      }
-    </View>
+      <Button title="SEND" onPress={sendMessageToAppleWatch} />
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {flex: 1, justifyContent: 'center', padding: 20},
-  todo: {marginBottom: 15},
-  input: {height: 50, backgroundColor: '#ddd', marginBottom: 10, padding:8},
-  todoName: {fontSize: 18},
+  container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
 });
 
-export default withAuthenticator(App);
+//export default withAuthenticator(App);
+export default App;
