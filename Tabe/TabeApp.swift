749c2d82589da2622bf045a93aa0191e29b107d5
Cole Berwick <colejberwick@gmail.com>
2020-12-07 13:54:43 -0500

    first commit

---
+++ Tabe/TabeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabeApp.swift
+//  Tabe
+//
+//  Created by Alex Saveliev on 12/7/20.
+//
+
+import SwiftUI
+
+@main
+struct TabeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
