a055ada0c843e3f25a5637424b96d9b3818eb13d
Cole Berwick <colejberwick@gmail.com>
2020-12-08 15:12:56 -0500

    added react navgiator

--- App.js
+++ App.js
@@ -1,33 +1,42 @@
 import React, {useState} from 'react';
 import {Text, TextInput, FlatList, View, Button, StyleSheet, Alert, SafeAreaView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import {withAuthenticator} from 'aws-amplify-react-native';
 
-const App = () => {
-    const [message, setMessage] = useState('');
-
+function HomeScreen(){
     const data = ['pee pee', 'poo poo', 'oooooh'];
-    const sendMessageToAppleWatch = () => {
-        Alert.alert(`the message "${message}" has been sent`);
-    };
-
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.top}></View>
             <View style={styles.box}>
                 <FlatList data={data} keyExtractor={item => item} renderItem={({item}) => <Text style={{color:'white'}}>{item}</Text>}>
-
                 </FlatList>
-
-
-
-
             </View>
             <View style={styles.bottom}>
-
             </View>
-
         </SafeAreaView>
+    );
+}
+
+const App = () => {
+    const [message, setMessage] = useState('');
+
+    const sendMessageToAppleWatch = () => {
+        Alert.alert(`the message "${message}" has been sent`);
+    };
+
+    const Tab = createBottomTabNavigator();
+
+
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={HomeScreen}/>
+                <Tab.Screen name="Settings" component={HomeScreen}/>
+            </Tab.Navigator>
+        </NavigationContainer>
     );
 };
 
