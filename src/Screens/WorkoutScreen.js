5bc772f9f89e097d039dfa8f6dd266acaf87b33f
Cole Berwick <colejberwick@gmail.com>
2020-12-08 19:44:41 -0500

    looks kinda cool

--- src/Screens/WorkoutScreen.js
+++ src/Screens/WorkoutScreen.js
@@ -23,28 +23,44 @@ const WorkoutScreen = () => {
     const Tab = createBottomTabNavigator();
 
     const workout = routine.days[routine.currentDay];
+    //best way is to just track it as it goes such as
+    /*
+    {
+        bench: [5,5,3,4]
+    }
+     */
 
     return (
         <SafeAreaView>
-            <Text style={{color: 'white'}}>{routine.title}</Text>
-            {
-                Object.entries(workout).map(([k,v]) =>
-                    <View>
-                        <Text style={{color: 'white'}}>{k}</Text>
-                        {
-                            v.map(n =>
-                                <Text style={{color: 'white'}}>{n}</Text>
-                            )
-                        }
-                    </View>
-                )
-            }
+            <View style={styles.top}>
+                <Text style={{color: 'black', fontSize: 20}}>{routine.title}</Text>
+            </View>
+            <View style={styles.container}>
+                {
+                    Object.entries(workout).map(([k,v]) => (
+                        <View style={styles.card} key={k}>
+                            <Text style={{color: 'white'}}>{k}</Text>
+                            <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
+                                {
+                                    v.map((n, index) =>
+                                        <View key={index} style={{width: 30, height: 30, justifyContent: 'center', alignItems: 'center', borderRadius: 100, borderWidth: 1, borderColor: primaryColor, borderStyle: 'solid'}}>
+                                            <Text key={index} style={{color: 'white'}}>{n}</Text>
+                                        </View>
+                                    )
+                                }
+                            </View>
+                        </View>
+                    ))
+                }
+            </View>
         </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: primaryColor},
+    title: {color: 'white'},
+    card: {margin: 5, borderRadius: 5, width: '100%', backgroundColor: '#333'},
+    container: {alignItems: 'center', justifyContent: 'center', margin: 5},
     top: {height: 40, width: '100%', backgroundColor: primaryColor, alignItems: 'center', borderStyle: 'solid', borderRightWidth: 0, borderLeftWidth: 0, borderTopWidth: 0, borderColor: 'black', borderWidth: 1, justifyContent: 'center'},
     box: {flex: 1, width: '100%', backgroundColor: 'black', alignItems: 'center', borderStyle: 'solid', borderColor: 'black', borderWidth: 1, justifyContent: 'center'},
     bottom: {height: 40, width: '100%', backgroundColor: 'orange', alignItems: 'center', borderStyle: 'solid', borderBottomWidth: 0, borderColor: 'black', borderWidth: 1, justifyContent: 'center'},
