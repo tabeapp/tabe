acc63848a85dd7e7490d1ede3c4b48aa24673810
Cole Berwick <colejberwick@gmail.com>
2020-12-09 10:21:35 -0500

    looks good

--- src/Utils/WeightVisual.js
+++ src/Utils/WeightVisual.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 //adjustable?
+//this is based on rouge plates
 const denoms = [
-    {weight: 45, style: {width: 20, height: '100%', backgroundColor: 'blue'}},
-    {weight: 25, style: {width: 10, height: '100%', backgroundColor: 'green'}},
-    {weight: 10, style: {width: 10, height: '50%', backgroundColor: 'white'}},
-    {weight: 5, style: {width: 10, height: '25%', backgroundColor: 'blue'}},
-    {weight: 2.5, style: {width: 10, height: '10%', backgroundColor: 'green'}},
+    {weight: 45, style: {width: 12, height: '100%', backgroundColor: 'blue'}},
+    {weight: 25, style: {width: 7, height: '100%', backgroundColor: 'green'}},
+    {weight: 10, style: {width: 5, height: '50%', backgroundColor: 'white'}},
+    {weight: 5, style: {width: 4, height: '42%', backgroundColor: 'blue'}},
+    {weight: 2.5, style: {width: 3, height: '36%', backgroundColor: 'green'}},
 ];
 
 const WeightVisual = props => {
@@ -16,8 +17,8 @@ const WeightVisual = props => {
     //bar
     weight -= 45;
     for(let i = 0; i < denoms.length; i++) {
-        while(weight > denoms[i].weight){
-            weight -= denoms[i].weight;
+        while(weight >= denoms[i].weight*2){
+            weight -= denoms[i].weight*2;
             info.push(denoms[i]);
         }
     }
@@ -27,7 +28,7 @@ const WeightVisual = props => {
     return (<View style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
         {
             info.map((i,index) =>
-                <View key={index} style={{...i.style, borderStyle: 'solid', borderWidth: 1, borderColor: 'white'}} />
+                <View key={index} style={{...i.style, borderStyle: 'solid', borderWidth: 1, borderColor: 'black'}} />
             )
         }
     </View>);
