ae7505aa8bec5beda4934018c90e64c183392f55
Cole Berwick <colejberwick@gmail.com>
2020-12-12 16:57:32 -0500

    probably the best we're gonna get

--- src/Components/NumericSelector.js
+++ src/Components/NumericSelector.js
@@ -1,8 +1,6 @@
 import React, {useState} from 'react';
-import SmoothPicker from 'react-native-smooth-picker';
-import { WrapperList, View, FlatList, ScrollView, Text } from "react-native";
-import { PRIMARY } from "../Constants/Theme";
-import { Picker } from "@react-native-picker/picker";
+import { PRIMARY } from '../Constants/Theme';
+import { Picker } from '@react-native-picker/picker';
 
 //im making my own, screw the libs
 //notes:
@@ -15,6 +13,7 @@ const NumericSelector = () => {
     //props will include a onchnage, which will updated teh reps or wahtever
     //props should also include min, max, increment
 
+    //const temp = Array.from({length: 200}, (_,i) => i*5);
     const temp = [];
     for(let i = 0; i < 1000; i += 5)
         temp.push(i);
@@ -24,35 +23,9 @@ const NumericSelector = () => {
             item
         }</Text>;
 
-
-
-    //const temp = Array.from({length: 200}, (_,i) => i*5);
-
-
-    //so fucking close, but also dumb as fuck
-    /*
+    //this has a weird gray box
     return (
-        <View style={{height: 80, borderRadius: 5, backgroundColor: 'white', width: 100}} >
-            <SmoothPicker
-                onSelected={({_, index}) => {console.log(index);setSelected(index+1)}}
-                snapInterval={50}
-                data={temp}
-                snapToAlignment={'center'}
-                startMargin={0.000001}//this is so fucking stupid
-                endMargin={0.000001}
-                renderItem={({item, index}) => {
-                    const color = index===selected?PRIMARY:'white';
-                    return (
-                        <Text key={item} style={{height: 50, backgroundColor: color, textAlign: 'center', fontSize: 40}}>{
-                            item
-                        }</Text>
-                    )
-                }}
-            />
-        </View>*/
-
-        //this has a weird gray box
-        return (<Picker
+        <Picker
             selectedValue={selected}
             style={{width: 100}}
             itemStyle={{fontSize: 40, borderRadius: 5, height: 70, backgroundColor:'white'}}
@@ -62,7 +35,7 @@ const NumericSelector = () => {
         >
             {
                 temp.map(item =>
-                    <Picker.Item label={''+item} value={item} style={{}}/>
+                    <Picker.Item color={selected===item?PRIMARY:'black'} label={''+item} value={item} style={{}}/>
                 )
             }
         </Picker>
