4542f6d9dff2d8d666bb6e557ec759acbacf3509
Cole Berwick <colejberwick@gmail.com>
2020-12-17 16:21:37 -0500

    adding just one new custom set works fine

--- src/Components/RepSchemeEditor.js
+++ src/Components/RepSchemeEditor.js
@@ -7,6 +7,12 @@ const reps = [];
 for(let i = 0; i <= 50; i++)
     reps.push(i)
 
+//yeah maybe I should make another compoenent for htis
+const percents = [];
+for(let i = 0; i <= 100; i+= 5)
+    percents.push(i)
+
+
 const RepSchemeEditor = props => {
     //i guess only one custom scheme per routine
     //otherwise how fucking complex is your workout?
@@ -15,13 +21,13 @@ const RepSchemeEditor = props => {
     return(
         <>
             <Words style={{fontSize: 40}}>Custom Rep Scheme</Words>
-            <Words>(workouts in this scheme will cycle through the following sets)</Words>
+            <Words>(workouts using this scheme will cycle through the following sets)</Words>
             <View style={{justifyContent: 'center', height: 200, margin: 5, width: 400, backgroundColor: '#333'}}>
                 {
-                    props.sets.map((week, index) =>
+                    props.sets.map((week, weekIndex) =>
                         <View style={{alignItems: 'center', flexDirection: 'row'}}>
-                            <Words>{index+1}:</Words>
-                            <View style={{flex: 1, height: 40, backgroundColor: 'black', borderRadius: 20}}>
+                            <Words>{weekIndex+1}:</Words>
+                            <View style={{flex: 1, height: 60, backgroundColor: 'transparent', borderRadius: 20}}>
                                 {
                                     <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
                                         <TouchableOpacity style={{margin: 5, height: 30, width: 30, justifyContent: 'center', alignItems: 'center', alignSelf: 'center', borderRadius: 15, borderWidth: 3, borderColor: 'red'}}
@@ -29,29 +35,60 @@ const RepSchemeEditor = props => {
                                             <Text style={{color: 'red', fontWeight: 'bold', fontSize: 15, }}>-</Text>
                                         </TouchableOpacity>
                                         {
-                                            props.sets.map((v, index) =>
+                                            week.map((v, setIndex) =>
 
-                                                <View key={index} style={styles.circle}>
-                                                    <Picker
-                                                        style={{width: 50, height: 50}}
-                                                        selectedValue={v}
-                                                        itemStyle={{fontSize: 20, borderRadius: 0, height: 50}}
-                                                        onValueChange={(value) => {
-                                                            //how the fuck
-                                                            //editExercise(value, 'setInfo', 'sets', index);
-                                                            //setProg(value);
-                                                        }}
-                                                    >
-                                                        {
-                                                            reps.map(item =>
-                                                                <Picker.Item key={item} color={'white'} label={''+item} value={item} style={{}}/> )
-                                                        }
-                                                    </Picker>
-                                                </View>
+                                                <>
+                                                    <View key={setIndex} style={styles.circle}>
+                                                        <Picker
+                                                            style={{width: 50, height: 50}}
+                                                            selectedValue={v.reps}
+                                                            itemStyle={{fontSize: 20, borderRadius: 0, height: 50}}
+                                                            onValueChange={(value) => {
+                                                            }}
+                                                        >
+                                                            {
+                                                                reps.map(item =>
+                                                                    <Picker.Item key={item} color={'white'} label={''+item} value={item} style={{}}/> )
+                                                            }
+                                                        </Picker>
+                                                    </View>
+                                                    <Words>@</Words>
+                                                    <View key={setIndex} style={styles.circle}>
+                                                        <Picker
+                                                            style={{width: 50, height: 50}}
+                                                            selectedValue={v['%']}
+                                                            itemStyle={{fontSize: 20, borderRadius: 0, height: 50}}
+                                                            onValueChange={(value) => {
+                                                            }}
+                                                        >
+                                                            {
+                                                                percents.map(item =>
+                                                                    <Picker.Item key={item} color={'white'} label={''+item} value={item} style={{}}/> )
+                                                            }
+                                                        </Picker>
+                                                    </View>
+                                                    <Words>%</Words>
+                                                </>
                                             )
                                         }
-                                        <TouchableOpacity style={{margin: 5, height: 30, width: 30, justifyContent: 'center', alignItems: 'center', alignSelf: 'center', borderRadius: 15, borderWidth: 3, borderColor: 'green'}}
-                                                          onPress={() => {}}>
+                                        <TouchableOpacity
+                                            style={{margin: 5, height: 30, width: 30, justifyContent: 'center', alignItems: 'center', alignSelf: 'center', borderRadius: 15, borderWidth: 3, borderColor: 'green'}}
+                                            onPress={() =>
+                                                props.edit(prev => {
+                                                    const next = [...prev];
+                                                    //weekIndex available
+                                                    if(next[weekIndex].length === 0)
+                                                        next[weekIndex] = [{reps:5, '%': 100}];
+                                                    else
+                                                        next[weekIndex].push([...next[weekIndex][next[weekIndex].length-1]]);
+
+                                                    return next;
+
+
+                                                })
+
+                                            }
+                                        >
                                             <Text style={{color: 'green', fontWeight: 'bold', fontSize: 15, }}>+</Text>
                                         </TouchableOpacity>
                                     </View>
