d6893ee2949c961224119b2d4bfc1ec1d2705dbd
Cole Berwick <colejberwick@gmail.com>
2020-12-17 11:48:31 -0500

    looks kinda like it, just need to drag stuff

--- src/Components/DaysEditor.js
+++ src/Components/DaysEditor.js
@@ -1,7 +1,7 @@
 import React, {useRef} from 'react';
 import { StyleSheet, Animated, PanResponder, ScrollView, Text, View } from "react-native";
 
-const Box = () => {
+const DraggableDay = props => {
     const pan = useRef(new Animated.ValueXY()).current;
     const panResponder = useRef(
         PanResponder.create({
@@ -17,62 +17,51 @@ const Box = () => {
     ).current;
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.titleText}>Drag & Release this box!</Text>
-            <Animated.View
-                style={{
-                    transform: [{ translateX: pan.x }, { translateY: pan.y }]
-                }}
-                {...panResponder.panHandlers}
-            >
-                <View style={{backgroundColor: 'red', width: 30, height: 30}}/>
-            </Animated.View>
-        </View>
-    );
-};
-
-const DraggableDay = props => {
-    const pan = useRef(new Animated.ValueXY()).current;
-    const panResponder = useRef(
-        PanResponder.create({
-            onMoveShouldSetPanResponder: () => {},
-            onPanResponderMove: Animated.event([
-                null,
-                {dx: pan.x, dy: pan.y}
-            ]),
-            onPanResponderRelease: () => {
-                Animated.spring(pan, {toValue: {x:0, y:0}
-                }).start()
-            }
-        })
-    ).current;
-
-    return (
         <Animated.View
             style={{
-                transform: [{translateX: pan.x}, {translateY: pan.y}]
+                transform: [{ translateX: pan.x }, { translateY: pan.y }],
             }}
             {...panResponder.panHandlers}
         >
-
+            <View style={{backgroundColor: 'red', width: 30, height: 30}}>
+                <Text>{props.value}</Text>
+            </View>
         </Animated.View>
-    )
-}
+    );
+};
 
 const DaysEditor = props => {
     return (<>
-        <View style={{ flexDirection: 'row' }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around', flexWrap: 'wrap'}}>
+            {
+                //just to mark days of week
+                ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].map(dow =>
+                    <View style={{ justifyContent: 'center', alignItems: 'center', width: '12.5%', height: 20, margin: 3 }} >
+                        <Text style={{color: 'white'}}>{dow}</Text>
+                    </View>
+                )
+            }
             {/*so this is like a schedule planner thing*/
                 //you have no idea how complex this is about to get
                 Array.from(new Array(14), () => null).map(d =>
-                    <View style={{ flex: 1, height: 20, backgroundColor: '#333', margin: 3 }} />
+                    //good enough lol
+                    <View style={{ justifyContent: 'center', alignItems: 'center', width: '12.5%', height: 30, backgroundColor: '#333', margin: 3 }} >
+                        {
+                            d===null &&
+                            <Text style={{color: '#811212'}}>R</Text>
+                        }
+                        {
+                            d!==null &&
+                            <Text style={{color: 'white'}}>{d}</Text>
+                        }
+                    </View>
                 )
             }
         </View>
-        <View style={{ flexDirection: 'row' }}>{
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around'}}>{
             //Object.keys(workouts).map(k =>
             ['A', 'B', 'C', 'D'].map(k =>
-                <Box/>
+                <DraggableDay value={k}/>
             )
 
         }</View>
@@ -80,12 +69,4 @@ const DaysEditor = props => {
 
 };
 
-const styles = StyleSheet.create({
-    box: {
-        height: 150,
-        width: 150,
-        backgroundColor: "blue",
-        borderRadius: 5
-    }
-});
 export default DaysEditor;
