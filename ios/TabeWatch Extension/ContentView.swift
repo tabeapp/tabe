cc3f4578c61403f37f698b2dc943522242bf30be
Cole Berwick <colejberwick@gmail.com>
2021-01-23 18:19:00 -0500

    watchos fucking sucks

--- ios/TabeWatch Extension/ContentView.swift
+++ ios/TabeWatch Extension/ContentView.swift
@@ -9,24 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Squat")
-                .font(.headline)
-                .lineLimit(0)
-            
-          
-            Divider()
-          
-            Text("5 sets")
-                .font(.caption)
-                .bold()
-                .lineLimit(0)
+        ScrollView {
+            VStack {
+                Text("Squat")
+                    .font(.headline)
+                    .lineLimit(0)
 
-            Text("5 reps")
-                .font(.caption)
-          
+
+                Divider()
+
+                Text("5 sets")
+                    .font(.caption)
+                    .bold()
+                    .lineLimit(0)
+
+                Text("5 reps")
+                    .font(.caption)
+
+            
+            }
         }
-       
+        .padding(16)
+
     }
 }
 
