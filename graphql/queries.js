828d651d54ca81b4d4ec4ec673bb216d0eab4a96
Cole Berwick <colejberwick@gmail.com>
2021-02-04 08:55:29 -0500

    messed around with graphql

--- graphql/queries.js
+++ graphql/queries.js
@@ -1,180 +1,14 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const getGroup = /* GraphQL */ `
-  query GetGroup($id: ID!) {
-    getGroup(id: $id) {
-      id
-      name
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const listGroups = /* GraphQL */ `
-  query ListGroups(
-    $filter: ModelGroupFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listGroups(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        name
-        createdAt
-        updatedAt
-      }
-      nextToken
-    }
-  }
-`;
-export const getRoutine = /* GraphQL */ `
-  query GetRoutine($id: ID!) {
-    getRoutine(id: $id) {
-      userID
-      id
-      title
-      data
-      current
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const listRoutines = /* GraphQL */ `
-  query ListRoutines(
-    $filter: ModelRoutineFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listRoutines(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        userID
-        id
-        title
-        data
-        current
-        createdAt
-        updatedAt
-      }
-      nextToken
-    }
-  }
-`;
-export const getUser = /* GraphQL */ `
-  query GetUser($id: ID!) {
-    getUser(id: $id) {
-      id
-      name
-      username
-      records
-      country
-      state
-      city
-      gym
-      posts {
-        nextToken
-      }
-      routines {
-        nextToken
-      }
-      currentWorkout {
-        id
-        data
-        createdAt
-        updatedAt
-      }
-      following {
-        id
-        name
-        username
-        records
-        country
-        state
-        city
-        gym
-        createdAt
-        updatedAt
-      }
-      followers {
-        id
-        name
-        username
-        records
-        country
-        state
-        city
-        gym
-        createdAt
-        updatedAt
-      }
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const listUsers = /* GraphQL */ `
-  query ListUsers(
-    $filter: ModelUserFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        name
-        username
-        records
-        country
-        state
-        city
-        gym
-        createdAt
-        updatedAt
-      }
-      nextToken
-    }
-  }
-`;
-export const getWorkout = /* GraphQL */ `
-  query GetWorkout($id: ID!) {
-    getWorkout(id: $id) {
-      id
-      data
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const listWorkouts = /* GraphQL */ `
-  query ListWorkouts(
-    $filter: ModelWorkoutFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listWorkouts(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        data
-        createdAt
-        updatedAt
-      }
-      nextToken
-    }
-  }
-`;
 export const getPost = /* GraphQL */ `
   query GetPost($id: ID!) {
     getPost(id: $id) {
+      type
       id
-      userID
-      title
-      description
-      data
-      gainz
-      comments {
-        nextToken
-      }
+      content
+      owner
+      timestamp
       createdAt
       updatedAt
     }
@@ -188,12 +22,11 @@ export const listPosts = /* GraphQL */ `
   ) {
     listPosts(filter: $filter, limit: $limit, nextToken: $nextToken) {
       items {
+        type
         id
-        userID
-        title
-        description
-        data
-        gainz
+        content
+        owner
+        timestamp
         createdAt
         updatedAt
       }
@@ -201,28 +34,59 @@ export const listPosts = /* GraphQL */ `
     }
   }
 `;
-export const getComment = /* GraphQL */ `
-  query GetComment($id: ID!) {
-    getComment(id: $id) {
-      id
-      postID
-      content
-      createdAt
-      updatedAt
-    }
-  }
-`;
-export const listComments = /* GraphQL */ `
-  query ListComments(
-    $filter: ModelCommentFilterInput
+export const listPostsSortedByTimestamp = /* GraphQL */ `
+  query ListPostsSortedByTimestamp(
+    $type: String
+    $timestamp: ModelIntKeyConditionInput
+    $sortDirection: ModelSortDirection
+    $filter: ModelPostFilterInput
     $limit: Int
     $nextToken: String
   ) {
-    listComments(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    listPostsSortedByTimestamp(
+      type: $type
+      timestamp: $timestamp
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
       items {
+        type
         id
-        postID
         content
+        owner
+        timestamp
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
+export const listPostsBySpecificOwner = /* GraphQL */ `
+  query ListPostsBySpecificOwner(
+    $owner: String
+    $timestamp: ModelIntKeyConditionInput
+    $sortDirection: ModelSortDirection
+    $filter: ModelPostFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listPostsBySpecificOwner(
+      owner: $owner
+      timestamp: $timestamp
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        type
+        id
+        content
+        owner
+        timestamp
         createdAt
         updatedAt
       }
