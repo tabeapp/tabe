6a7b54c0bb79c4ed40f219ee01dce37cf450c808
Cole Berwick <colejberwick@gmail.com>
2021-02-03 22:36:17 -0500

    messing around with graphql updating, come back next time and do amplify push

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { TouchableOpacity, FlatList, Alert, SafeAreaView, StyleSheet, View } from "react-native";
+import { TextInput, TouchableOpacity, FlatList, Alert, SafeAreaView, StyleSheet, View } from "react-native";
 import React, {useState, useContext, useEffect} from 'react';
 import NavBar from '../Components/NavBar';
 import { PRIMARY } from '../Constants/Theme';
@@ -9,7 +9,12 @@ import TopBar from "../Components/TopBar";
 import Row from "../Components/Row";
 import { STYLES } from "../Style/Values";
 import WorkoutContext from "../Contexts/WorkoutContext";
+import { withAuthenticator } from "aws-amplify-react-native";
+import Write from "../Components/Write";
+import { API, Auth, graphqlOperation } from "aws-amplify";
+import { createPost } from "../../graphql/mutations";
 
+//https://amplify-sns.workshop.aws/en/30_mock/30_post_front_end.html
 const HomeScreen = props => {
     //what's the best way to load
     //yeah we're gonna need a Social context
@@ -23,10 +28,54 @@ const HomeScreen = props => {
     //getPosts().then(v => setPosts(v))
     //})
 
+    const [value, setValue] = useState('eyyyy');
+
+    const onPost = async () => {
+        const res = await API.graphql(graphqlOperation(createPost, {
+            input: {
+                type: 'post',
+                content: value,
+                timestamp: Date.now()
+            }}));
+        console.log(res);
+        setValue('')
+    };
+
+    //not doing this here lol
+    const signOut = () => {
+        /*Auth.signOut()
+            .then(data => console.log(data))
+            .catch(err => console.log(err));*/
+    };
+
     return (
         <SafeBorderNav {...props} screen={'home'}>
             <TopBar title='Feed'/>
             <View style={STYLES.body}>
+                <Write value={value} onChange={v => {
+                    //arbitrary limit lol
+                    if(v.length <= 140)
+                        setValue(v)
+                }}/>
+                <TouchableOpacity style={{backgroundColor: 'blue'}} onPress={onPost} >
+                    <Words>Post</Words>
+                </TouchableOpacity>
+                <Words/>
+                <Words/>
+                <Words/>
+                <Words/>
+                <TouchableOpacity style={{backgroundColor: 'red'}} onPress={signOut} >
+                    <Words>Sign out</Words>
+                </TouchableOpacity>
+            </View>
+        </SafeBorderNav>
+    );
+    /*
+    return (
+        <SafeBorderNav {...props} screen={'home'}>
+            <TopBar title='Feed'/>
+            <View style={STYLES.body}>
+                <Words>Hello posts</Words>
                 <FlatList data={posts} keyExtractor={item => ''+item.time} renderItem={({item}) =>
                     item.exercises &&
                     <TouchableOpacity
@@ -50,7 +99,35 @@ const HomeScreen = props => {
                 />
             </View>
         </SafeBorderNav>
-    );
+    );*/
 };
 
-export default HomeScreen;
+const drawerWidth = 340;
+const MAX_POST_CONTENT_LENGTH = 140;
+const styles = StyleSheet.create({
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+        position: 'relative',
+    },
+    drawerPaper: {
+        width: drawerWidth,
+        position: 'relative',
+    },
+    //toolbar: theme.mixins.toolbar,
+    textField: {
+        width: drawerWidth,
+    },
+    list: {
+        // overflowWrap: 'break-word',
+        width: 300,
+    },
+});
+
+export default withAuthenticator(HomeScreen, {
+    signUpConfig:{
+        hiddenDefaults: ['phone_number']
+
+    }
+
+});
