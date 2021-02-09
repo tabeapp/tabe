79881c2541ae822f4a3aeb63ae07c6ef78e68e5d
Cole Berwick <colejberwick@gmail.com>
2021-02-09 15:23:53 -0500

    added video

--- src/Screens/TutorialScreen.js
+++ src/Screens/TutorialScreen.js
@@ -1,17 +1,27 @@
-import React, {useState, useReducer, useEffect} from 'react';
-import { FlatList, TouchableOpacity, StyleSheet, View } from 'react-native';
-import Words from "../Components/Words";
-import SafeBorderNav from "../Components/SafeBorderNav";
-import TopBar from "../Components/TopBar";
-import { STYLES } from "../Style/Values";
+import React from 'react';
+import { View } from 'react-native';
+import Words from '../Components/Words';
+import SafeBorderNav from '../Components/SafeBorderNav';
+import TopBar from '../Components/TopBar';
+import { STYLES } from '../Style/Values';
+
+const Post = () => {
+    return (
+        <View>
+            <Words>Post</Words>
+        </View>
+    )
+};
 
 //https://www.youtube.com/watch?v=TGg9WNLUZPc
+//also not bad https://www.youtube.com/watch?v=nvB3ZhHGsOE
 const TutorialScreen = props => {
     return (
         <SafeBorderNav {...props} screen={'explore'}>
             <TopBar title='Explore'/>
             <View style={STYLES.body}>
-                <Words>Shit</Words>
+                <Words>Home Screen</Words>
+                <Post/>
             </View>
         </SafeBorderNav>
     );
