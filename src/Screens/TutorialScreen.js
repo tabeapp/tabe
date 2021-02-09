3efbf83183799b391605cb087b88529738c2749d
Cole Berwick <colejberwick@gmail.com>
2021-02-09 16:13:39 -0500

    cool vid

--- src/Screens/TutorialScreen.js
+++ src/Screens/TutorialScreen.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {useState} from 'react';
+import { View, Dimensions, TouchableWithoutFeedback } from 'react-native';
 import Words from '../Components/Words';
 import SafeBorderNav from '../Components/SafeBorderNav';
 import TopBar from '../Components/TopBar';
 import { STYLES } from '../Style/Values';
+import Video from 'react-native-video';
 
 const Post = () => {
+    const [paused, setPaused] = useState(false);
+
+    const onPlayPausePress = () => setPaused(prev => !prev);
+
     return (
-        <View>
-            <Words>Post</Words>
+        <View
+            style={{
+                width: '100%',
+                height: Dimensions.get('window').height
+            }}
+        >
+            <TouchableWithoutFeedback onPress={onPlayPausePress}>
+                <Video
+                    source={{uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4'}}
+                    style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        bottom: 0,
+                        right: 0
+                    }}
+                    onError={e => console.log(e)}
+                    resizeMode={'cover'}
+                    repeat={true}
+                    paused={paused}
+                />
+            </TouchableWithoutFeedback>
         </View>
     )
 };
