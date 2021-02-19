900ef1c043ce1e81380bdcb364555bf4ad7aed45
Cole Berwick <colejberwick@gmail.com>
2021-02-18 21:04:26 -0500

    changing current routine does work, just needs to have a subscription set up

--- src/Screens/RoutineScreen.js
+++ src/Screens/RoutineScreen.js
@@ -11,39 +11,43 @@ import SafeBorder from "../Components/SafeBorder";
 import SafeBorderNav from "../Components/SafeBorderNav";
 import TopBar from "../Components/TopBar";
 import { STYLES } from "../Style/Values";
+import { Routine } from "../../models";
+import { API, DataStore, graphqlOperation } from "aws-amplify";
+import { updateRoutine } from "../../graphql/mutations";
+import { UserContext } from "../Contexts/UserProvider";
 
 //this is for choosing a routine to edit, instead of jumping right in
 //crud operations on this level deserve server calls
 const RoutineScreen = props => {
     //why is it like this
+    const {username} = useContext(UserContext);
     const {routines} = useContext(RoutinesContext);
     const {routinesDispatch} = useContext(RoutinesContext);
 
-    const deleteRoutine = k => {
-        //should probably have a warning, lol
-        routinesDispatch(prev => {
-            delete prev.routines[k];
-            return prev;
+    const deleteRoutine = async id => {
+        //datastore actually makes this simple
+        const toDelete = await DataStore.query(Routine, id);
+        DataStore.delete(toDelete);
+    };
+
+    const handleSetCurrent = async id => {
+        //would this be easier with graphql mutation?
+        //no
+        //this unfortutaely is the best way
+        //pull all the users routines and set them current zero except the one
+        const usersRoutines = await DataStore.query(Routine, r => r.userID('eq', username));
+        usersRoutines.forEach(routine => {
+            //only two secnarios that need to be updated
+            if(routine.current === 1 && routine.id !== id)
+                DataStore.save(Routine.copyOf(routine, updated => {
+                    updated.current = 0
+                }));
+            else if(routine.current === 0 && routine.id === id)
+                DataStore.save(Routine.copyOf(routine, updated => {
+                    updated.current = 1
+                }));
         });
-        //not a fan, but this should be a rare operation
-        routinesDispatch({type: 'setItem'})//so save the thing
-    }
-
-    //again we have to fucking update the @rouintes
-    const handleSetCurrent = k => {
-        //setCurrent(k);
-        routinesDispatch({path: 'current', value: k});
-
-        //AsyncStorage.getItem('@routines').then(obj => {
-        //if(obj === null)
-        //return;
-        //const r = JSON.parse(obj);
-        //r.current = k;
-        routinesDispatch({type: 'setItem'})//so save the thing
-        //AsyncStorage.setItem('@routines', JSON.stringify(r));
-        //})
-
-    }
+    };
 
     return (
         <SafeBorderNav {...props} screen={'routine'}>
