5779288f6e12c54f0055dd4898057421b1f381d5
Cole Berwick <colejberwick@gmail.com>
2021-02-26 21:30:34 -0500

    fake gym pops up on map for first time

--- src/Screens/GymMapScreen.js
+++ src/Screens/GymMapScreen.js
@@ -4,7 +4,7 @@ import SafeBorderNav from '../Components/Navigation/SafeBorderNav';
 import TopBar from '../Components/Navigation/TopBar';
 import { STYLES } from '../Style/Values';
 import { API, Auth, graphqlOperation } from 'aws-amplify';
-import { listPostsSortedByTimestamp } from '../../graphql/queries';
+import { listPostsSortedByTimestamp, nearbyGyms, searchGyms } from '../../graphql/queries';
 import { onCreatePost } from '../../graphql/subscriptions';
 import PostList from '../Components/Social/PostList';
 import Geolocation from '@react-native-community/geolocation';
@@ -13,19 +13,16 @@ import SafeBorder from '../Components/Navigation/SafeBorder';
 
 MapBoxGL.setAccessToken('pk.eyJ1IjoidGFiZWFwcCIsImEiOiJja2xuMjUwYjUwZXlyMnNxcGt2MG5scnBuIn0.azxOspBiyh1cbe3xtIGuLQ');
 
-const SUBSCRIPTION = 'SUBSCRIPTION';
-const INITIAL_QUERY = 'INITIAL_QUERY';
-const ADDITIONAL_QUERY = 'ADDITIONAL_QUERY';
-
+const ADD_GYMS = 'ADD_GYMS';
 
 const reducer = (state, action) => {
     switch(action.type){
-        case INITIAL_QUERY:
-            return action.posts;
-        case ADDITIONAL_QUERY:
-            return [...state, ...action.posts];
-        case SUBSCRIPTION:
-            return [action.post, ...state];
+        case ADD_GYMS:
+            action.gyms.forEach(gym => {
+                state[gym.id] = gym;
+            });
+            return state;
+
         default:
             return state;
     }
@@ -34,73 +31,77 @@ const reducer = (state, action) => {
 //https://amplify-sns.workshop.aws/en/30_mock/30_post_front_end.html
 const GymMapScreen = props => {
     //not doing this here lol
-    const signOut = () => {
-        Auth.signOut()
-            .then(data => console.log(data))
-            .catch(err => console.log(err));
-    };
+    const [gyms, dispatch] = useReducer(reducer, {});
 
-    const [posts, dispatch] = useReducer(reducer, []);
-    const [nextToken, setNextToken] = useState(null);
+    //const [posts, dispatch] = useReducer(reducer, []);
     const [isLoading, setIsLoading] = useState(true);
 
-    const getPosts = async (type, nextToken = null) => {
-        const res = await API.graphql(graphqlOperation(listPostsSortedByTimestamp, {
-            type: "post",
-            sortDirection: 'DESC',
-            limit: 20, //default = 10
-            nextToken: nextToken,
-        }));
-        console.log(res);
-        dispatch({ type: type, posts: res.data.listPostsSortedByTimestamp.items })
-        setNextToken(res.data.listPostsSortedByTimestamp.nextToken);
-        setIsLoading(false);
-    }
-
-    const getAdditionalPosts = () => {
-        if (nextToken === null) return; //Reached the last page
-        getPosts(ADDITIONAL_QUERY, nextToken);
-    }
-
-    useEffect(() => {
-        getPosts(INITIAL_QUERY);
-
-        const subscription = API.graphql(graphqlOperation(onCreatePost)).subscribe({
-            next: (msg) => {
-                console.log('allposts subscription fired')
-                const post = msg.value.data.onCreatePost;
-                dispatch({ type: SUBSCRIPTION, post: post });
-            }
-        });
-        return () => subscription.unsubscribe();
-    }, []);
-
     const [coordinates, setCoordinates] = useState({latitude: 45, longitude: 70});
+
+    //no subscription, just search whenever coords change
+    useEffect(() => {
+        setIsLoading(true);
+        API.graphql(graphqlOperation(nearbyGyms, {
+            location: {
+                lat: coordinates.latitude,
+                lon: coordinates.longitude
+            },
+            km: 20
+
+        }))
+            .then(results => {
+                console.log(results);
+                dispatch({type: ADD_GYMS, gyms: results.data.nearbyGyms.items});
+                setIsLoading(false);
+            });
+
+    }, [coordinates]);
 
     useEffect(() => {
         Geolocation.getCurrentPosition(info => {
             setCoordinates({
                 latitude: info.coords.latitude,
                 longitude: info.coords.longitude
-            })
+            });
+
         });
     }, []);
+
     return (
         <SafeBorder {...props} >
             <TopBar title='Gym Map'/>
             <View style={STYLES.body}>
-                <MapBoxGL.MapView style={{flex:1, width: '100%'}}/>
+                <MapBoxGL.MapView
+                    style={{flex:1, width: '100%'}}
+                    styleURL={MapBoxGL.StyleURL.Dark}
+                    showUserLocation={true}
+                    centerCoordinate={[coordinates.longitude, coordinates.latitude]}
+                    zoomLevel={14}
+                >
+                    <MapBoxGL.Camera
+                        centerCoordinate={[coordinates.longitude, coordinates.latitude]}
+                        zoomLevel={14}
+                        animationDuration={0}
+                    />
+                    <MapBoxGL.PointAnnotation id={'me'} coordinate={[coordinates.longitude, coordinates.latitude]}>
+                        <View style={{height: 30, width: 30, backgroundColor: 'blue'}}/>
+                    </MapBoxGL.PointAnnotation>
+                    {
+                        gyms&&
+                        Object.values(gyms).map(gym =>
+                            <MapBoxGL.PointAnnotation id={gym.id} coordinate={[gym.location.lon, gym.location.lat]}>
+                                <View style={{height: 30, width: 30, backgroundColor: 'green'}}/>
+                            </MapBoxGL.PointAnnotation>
+
+                        )
+                    }
+
+                </MapBoxGL.MapView>
+
             </View>
         </SafeBorder>
     );
 };
 
-
-const styles = StyleSheet.create({
-    list: {
-        // overflowWrap: 'break-word',
-        width: 300,
-    },
-});
 
 export default GymMapScreen;
